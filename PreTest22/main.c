2ea8185718cf757625f1e23a24cea8d08545ba66
DressAnDy <155094042+DressAnDy@users.noreply.github.com>
2024-03-06 20:35:57 +0700

    Update main.c

--- PreTest22/main.c
+++ PreTest22/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+void splice(char str[], int pos , int amount);
 int main()
 {
-    printf("Hello world!\n");
+    char str[100] = "xin chao ban";
+    splice(str , 4 ,3);
+    printf("%s", str);
     return 0;
+}
+void splice(char str[], int pos , int amount){
+    int size = strlen(str);
+    for(int i = pos ; i <= strlen(str) - 1 ; i++){
+        str[i] = str[i + amount];
+    }
+    amount = size - pos >= amount ? amount : size - pos;
+    str[size - amount] = '\0';
 }
